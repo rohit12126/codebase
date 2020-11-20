e1ad99e96fa57a167e3fe45535aa4b8052136d73
Rohit <rohita.chapter247@gmail.com>
2020-11-20 14:37:56 +0530

    Prevent admin to login in front user

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+// use GuzzleHttp\Psr7\Request;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\MessageBag;
 
 class LoginController extends Controller
 {
@@ -57,5 +60,25 @@ class LoginController extends Controller
         $user = Socialite::driver('facebook')->user();
 
         // $user->token;
+    }
+
+    protected function getCredentials(Request $request)
+    {
+        return [
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+            'menuroles' => 'user'
+        ];
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, ['email' => 'required|email', 'password' => 'required']);
+
+        if (\Auth::attempt($this->getCredentials($request))) {
+            return redirect()->route('account');
+        }
+        $errors = new MessageBag(['password' => ['These credentials do not match our records.']]); 
+        return redirect()->back()->withErrors($errors);
     }
 }
