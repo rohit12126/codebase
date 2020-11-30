0d7a6942c33baed6476f7c9f2f774d59c81e2431
harishm247 <harish.chapter247@gmail.com>
2020-11-30 14:11:49 +0530

    changes

--- resources/views/dashboard/page_list.blade.php
+++ resources/views/dashboard/page_list.blade.php
@@ -12,105 +12,31 @@
 
 <div class="container-fluid">
     <div class="fade-in">
-         <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
-                    <div class="card-header">
-                    <div class="row">
-                        <div class="col-8">
-                        <h4>Product Filter</h4>
-                        </div>
-                        <div class="col-4">
-                        <form method="POST" action="{{ url()->current() }}" enctype="multipart/form-data">
-                            @csrf
-                        <button type="submit" class="btn btn-primary" style="float: right;">
-                                Submit
-                            </button>
-                            @if(@$_POST)
-                            <a href="{{route('admin.product.list')}}" class="btn btn-danger text-white">
-                                Clear Active Filters
-                            </a>
-                            @endif
-                        </div>
-                    </div>
-
-                    </div>
-                    <div class="card-body">
-                        @include('partials.alert_msg')
-                        
-                            <input type="hidden" name="page" id="page" value="" />
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col">
-                                    <label>Product Name</label>
-                                    <input type="text" placeholder="Product Name" name="product_name"
-                                        class="form-control" value="{{ @$_POST['product_name'] }}">
-                                    </div>
-                                    {{-- <div class="col">
-                                    <label>Stock Less Quantity</label>
-                                    <input type="text" placeholder="Stock Less Quantity" name="stock_less" class="form-control"
-                                        value="{{ @$_POST['stock_less'] }}" autocomplete="off">
-                                    </div> --}}
-                                    <div class="col">
-                                    <label>Status</label>
-                                    <select name="product_status" id="" class="form-control">
-                                        <option value="">Select Status</option>
-                                        <option value="1" @if(@$_POST['product_status']==1) selected @endif </option>Active
-                                        </option>
-                                        <option value="0" @if(@$_POST['product_status']==2) selected @endif </option>In-Active
-                                        </option>
-                                    </select>
-                                    </div>
-                                </div>
-                                </div>
-                         
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
         <div class="row">
             <div class="col-sm-12">
                 <div class="card">
                     <div class="card-header">
-                        <h4>Product List</h4>
+                        <h4>Page List</h4>
                     </div>
                     <div class="card-body">
                         <table class="table table-striped table-bordered datatable">
                             <thead>
                                 <tr>
                                     <th>S No.</th>
-                                    <th>Name</th>
-                                    <th>Stock Quantity</th>
-                                    <th>Purchase Price</th>
-                                    <th>Sale Price</th>
-                                    <th>Image</th>
-                                    <th>Action</th>
-
+                                    <th>Title</th>
                                 </tr>
                             </thead>
                             <tbody>
 
-                                @foreach($product_list as $key => $value)
+                                @foreach($page_list as $key => $value)
                                 <tr>
                                     <td>{{ $key+1 }}</td>
-                                    <td>{{ $value->name }}</td>
-                                    <td>{{ $value->stock_qty }}</td>
-                                    <td>{{ $value->purchase_price }}</td>
-                                    <td>{{ $value->sale_price }}</td>
+                                    <td>{{ $value->title }}</td>
                                     <td>
-                                        @if(count($value->images) > 0)
-                                        @foreach($value->images as $key2 => $value2)
-                                        <img src="{{ url('') }}/upload/product/{{ @$value2->image}}" width="50" />
-                                        @endforeach
-                                        @endif
-                                    </td>
-                                    <td>
-                                        <a class="btn btn-info" href="{{ url('admin/edit_product', $value->id) }}" title="Edit">
+                                        <a class="btn btn-info" href="{{ url('admin/edit_cms', $value->id) }}" title="Edit">
                                             <i class="cil-pencil"></i>
                                         </a>
-                                        <a class="btn btn-danger" href="{{ url('admin/delete_product', $value->id) }}" onclick="return confirm('Are you sure you want to delete this product?');" title="Delete">
+                                        <a class="btn btn-danger" href="{{ url('admin/delete_page', $value->id) }}" onclick="return confirm('Are you sure you want to delete this page?');" title="Delete">
                                             <i class="cil-trash"></i>
                                         </a>
                                     </td>
