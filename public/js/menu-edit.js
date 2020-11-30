4e062df74aaca7cbd8cf5864aab68eb4f3f836d0
Ananad Jain <anand.chapter247@gmail.com>
2020-11-30 17:58:57 +0530

    nerges

--- public/js/menu-edit.js
+++ public/js/menu-edit.js
@@ -161,7 +161,7 @@ document.getElementById("type").onchange = function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /var/www/html/c247/codebase/resources/js/coreui/menu-edit.js */"./resources/js/coreui/menu-edit.js");
+module.exports = __webpack_require__(/*! /var/www/html/codebase/resources/js/coreui/menu-edit.js */"./resources/js/coreui/menu-edit.js");
 
 
 /***/ })
