368ed41076fba7b8aae633a341b8750ccba6f6b5
rohit-chapter <rohit.chapter247@gmail.com>
2020-12-03 18:03:23 +0530

    changes for design

--- resources/views/dashboard/homepage.blade.php
+++ resources/views/dashboard/homepage.blade.php
@@ -20,71 +20,109 @@
     <div class="fade-in">
         <div class="row">
             <div class="col-sm-6 col-lg-3">
-                <div class="card text-white bg-primary cursor-pointer" title="Active Users" onclick="window.location='{{ route('admin.user') }}'">
+                <div class="card text-white cursor-pointer dashboard-block first-block" title="Active Users" onclick="window.location='{{ route('admin.user') }}'">
                     <div class="card-body pb-0">
-                        <div class="text-value-lg">{{ $data['userCount'] }}</div>
-                        <div>Total Users</div>
+                        <div class="d-flex">
+                            <div class="icons-block">
+                                <i class="cil-user"></i>
+                            </div>
+                            <div>
+                                <div class="text-value-lg">{{ $data['userCount'] }}</div>
+                                <div>Total Users</div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="c-chart-wrapper mt-3 mx-3" style="height:70px;">
-                    </div>
+                    <!-- <div class="c-chart-wrapper mt-3 mx-3" style="height:70px;">
+                    </div> -->
                 </div>
             </div>
             <!-- /.col-->
             <div class="col-sm-6 col-lg-3">
-                <div class="card text-white bg-info cursor-pointer" title="Total Orders" onclick="window.location='{{ route('admin.order') }}'">
+                <div class="card text-white cursor-pointer dashboard-block second-block" title="Total Orders" onclick="window.location='{{ route('admin.order') }}'">
                     <div class="card-body pb-0">
-                        <div class="text-value-lg">{{ $data['orderCount'] }}</div>
-                        <div>Total Orders</div>
+                      <div class="d-flex">
+                        <div class="icons-block">
+                            <i class="cil-user"></i>
+                        </div>
+                        <div>
+                            <div class="text-value-lg">{{ $data['orderCount'] }}</div>
+                                <div>Total Orders</div>
+                            </div>
+                       </div>
                     </div>
-                    <div class="c-chart-wrapper mt-3 mx-3" style="height:70px;">
-                    </div>
+                    <!-- <div class="c-chart-wrapper mt-3 mx-3" style="height:70px;"></div> -->
                 </div>
             </div>
             <!-- /.col-->
            
             <div class="col-sm-6 col-lg-3">
-                <div class="card text-white bg-warning cursor-pointer" id="activeProduct">
+                <div class="card text-white cursor-pointer dashboard-block third-block" id="activeProduct">
                     <div class="card-body pb-0">
-                        <div class="text-value-lg">{{ $data['productsCount'] }}</div>
-                        <div>Total Products</div>
+                        <div class="d-flex">
+                            <div class="icons-block">
+                                <i class="cil-user"></i>
+                            </div>
+                            <div>
+                                <div class="text-value-lg">{{ $data['productsCount'] }}</div>
+                                <div>Total Products</div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="c-chart-wrapper mt-3" style="height:70px;">
-                    </div>
+                    <!-- <div class="c-chart-wrapper mt-3" style="height:70px;"></div> -->
                 </div>
             </div>
           
             <!-- /.col-->
             <div class="col-sm-6 col-lg-3">
-              <div class="card text-white bg-primary cursor-pointer" title="Total Active Accessories" onclick="window.location='{{ route('admin.item.list') }}'">
+              <div class="card text-white cursor-pointer dashboard-block fourth-block" title="Total Active Accessories" onclick="window.location='{{ route('admin.item.list') }}'">
                   <div class="card-body pb-0">
-                    <div class="text-value-lg">{{ $data['accessoryCount'] }}</div>
-                    <div>Total Accessories</div>
+                    <div class="d-flex">
+                            <div class="icons-block">
+                                <i class="cil-user"></i>
+                            </div>
+                            <div>
+                                <div class="text-value-lg">{{ $data['accessoryCount'] }}</div>
+                                <div>Total Accessories</div>
+                            </div>
+                        </div>
                   </div>
-                  <div class="c-chart-wrapper mt-3 mx-3" style="height:70px;">
-                  </div>
+                  <!-- <div class="c-chart-wrapper mt-3 mx-3" style="height:70px;"></div> -->
               </div>
           </div>
             <!-- /.col-->
             <div class="col-sm-6 col-lg-3">
-                <div class="card text-white bg-danger cursor-pointer" title="Total Cancelled Orders" onclick="window.location='{{ route('admin.order') }}'">
+                <div class="card text-white cursor-pointer dashboard-block fifth-block" title="Total Cancelled Orders" onclick="window.location='{{ route('admin.order') }}'">
                     <div class="card-body pb-0">
-                        <div class="text-value-lg">{{ $data['cancleOrderCount'] }}</div>
-                        <div>Total Cancelled Orders</div>
+                    <div class="d-flex">
+                        <div class="icons-block">
+                                <i class="cil-user"></i>
+                        </div>
+                        <div>
+                            <div class="text-value-lg">{{ $data['cancleOrderCount'] }}</div>
+                            <div>Total Cancelled Orders</div>
+                        </div>
                     </div>
-                    <div class="c-chart-wrapper mt-3 mx-3" style="height:70px;">
+                    </div>
+                    <!-- <div class="c-chart-wrapper mt-3 mx-3" style="height:70px;">
                         <canvas class="chart" id="card-chart4" height="70"></canvas>
-                    </div>
+                    </div> -->
                 </div>
             </div>
             <!-- /.col-->
             <div class="col-sm-6 col-lg-3">
-              <div class="card text-white bg-warning cursor-pointer" title="Total Active Blogs" onclick="window.location='{{ route('admin.blog.list') }}'">
+              <div class="card text-white cursor-pointer dashboard-block six-block" title="Total Active Blogs" onclick="window.location='{{ route('admin.blog.list') }}'">
                   <div class="card-body pb-0">
-                      <div class="text-value-lg">{{ $data['blogCount'] }}</div>
-                      <div>Total Blogs</div>
+                    <div class="d-flex">
+                        <div class="icons-block">
+                            <i class="cil-user"></i>
+                        </div>
+                        <div>
+                            <div class="text-value-lg">{{ $data['blogCount'] }}</div>
+                            <div>Total Blogs</div>
+                        </div>
+                    </div>
                   </div>
-                  <div class="c-chart-wrapper mt-3" style="height:70px;">
-                  </div>
+                  <!-- <div class="c-chart-wrapper mt-3" style="height:70px;"></div> -->
               </div>
           </div>
         </div>
