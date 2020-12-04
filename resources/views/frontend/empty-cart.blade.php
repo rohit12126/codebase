8873db33fde1aab034a26c936d96685590cd5754
harishm247 <harish.chapter247@gmail.com>
2020-12-04 11:26:49 +0530

    H2 added to cart empty page

--- resources/views/frontend/empty-cart.blade.php
+++ resources/views/frontend/empty-cart.blade.php
@@ -5,7 +5,8 @@
 	<div class="container">
         <div class="row">
             <div class="col-12">
-                Cart Is Empty
+                <h2>Cart Is Empty</h2>
+                <img src="" alt="">
             </div>
         </div>
     </div>
