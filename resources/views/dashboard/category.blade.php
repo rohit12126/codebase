93e85bf060e44c5977f28b75b945c12c16fd0402
harishm247 <harish.chapter247@gmail.com>
2020-12-10 17:32:11 +0530

    breadcrump

--- resources/views/dashboard/category.blade.php
+++ resources/views/dashboard/category.blade.php
@@ -8,7 +8,8 @@
     <div class="c-subheader px-3">
         <ol class="breadcrumb border-0 m-0">
             <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-            <li class="breadcrumb-item"><a href="{{ route('admin.category') }}">Category</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('admin.item.list') }}">Items</a></li>
+            <li class="breadcrumb-item active"><a href="{{ route('admin.category') }}">Category</a></li>
         </ol>
     </div>
 @endsection
