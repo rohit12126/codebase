aea24f16e75df4da76c3126496c65d87245583df
rohit-chapter <rohit.chapter247@gmail.com>
2020-12-12 11:57:11 +0530

    designing changes

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -114,6 +114,15 @@
                                     {{ $errors->first('email') }}
                                 </div>
                                 @endif
+                            </div>                           
+                            <div class="form-group col-md-12 custom-selectbox">
+                                <span class="linearicons-chevron-down"></span>
+                                <select class="form-control">
+                                    <option selected>Open this select menu</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
                             </div>
                             <div class="form-group col-md-12">
                                 <textarea required="" placeholder="Message *" id="description" class="form-control {{ $errors->has('message') ? 'error' : '' }}" name="message" rows="4"></textarea>
