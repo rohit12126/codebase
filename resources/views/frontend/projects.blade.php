22085efa4e69b26920382222696edae980eff2e8
harishm247 <rohit.chapter247@gmail.com>
2020-12-15 18:05:45 +0530

    projects page update

--- resources/views/frontend/projects.blade.php
+++ resources/views/frontend/projects.blade.php
@@ -1,94 +1,99 @@
 @extends('layouts.front')
 @section('content')
-<section class="section light-bg">
+<section class="section projects-wrap">
         <div class="container">
-            <h1 class="text-center mb-5">Portfolio</h1>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="portfolio-tile">
-                        <span class="portfolio-category">
-                            portfolio category
-                        </span>
-                        <div class="portfolio-title-div">
-                            <a href="">
-                                <span class="portfolio-title">
-                                    Portfolio Title BB Roy of great Britain
-                                </span>
-                            </a>
-                        </div>
-                        <a href="" class="portfolio-img">
-                            <img src="{{URL::asset('/images/portfolio/portfolio1.jpg')}}" alt="">
-                        </a>    
-                    </div>
+            <h1 class="text-center mb-5">Projects</h1>
+            <div class="blog-row justify-content-center">
+                <div class="blog-col">
+                    <a href="">
+                        <article class="blog-list-card">
+                            <div class="blog-list-title">
+                                Custom closet project one
+                            </div>
+                            <img src="{{URL::asset('/images/projects/projects1.jpg')}}" alt="" class="blog-img">
+                            <div class="blog-foot">
+                                <a href="">
+                                    <span class="blog-view-button">View</span>
+                                </a>
+                            </div>
+                        </article>
+                    </a>
+                </div>   
+                <div class="blog-col">
+                    <a href="">
+                        <article class="blog-list-card">
+                            <div class="blog-list-title">
+                                Custom closet project two lorem ipsum
+                            </div>
+                            <img src="{{URL::asset('/images/projects/projects2.jpg')}}" alt="" class="blog-img">
+                            <div class="blog-foot">
+                                <a href="">
+                                    <span class="blog-view-button">View</span>
+                                </a>
+                            </div>
+                        </article>
+                    </a>
                 </div>
-                <div class="col-md-6">
-                    <div class="portfolio-tile">
-                        <span class="portfolio-category">
-                            portfolio category
-                        </span>
-                        <div class="portfolio-title-div">
-                            <a href="">
-                                <span class="portfolio-title">
-                                    Portfolio Title BB Roy of great Britain
-                                </span>
-                            </a>
-                        </div>
-                        <a href="" class="portfolio-img">
-                            <img src="{{URL::asset('/images/portfolio/portfolio2.jpg')}}" alt="">
-                        </a>    
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="portfolio-tile">
-                        <span class="portfolio-category">
-                            portfolio category
-                        </span>
-                        <div class="portfolio-title-div">
-                            <a href="">
-                                <span class="portfolio-title">
-                                    Portfolio Title BB Roy of great Britain
-                                </span>
-                            </a>
-                        </div>
-                        <a href="" class="portfolio-img">
-                            <img src="{{URL::asset('/images/portfolio/portfolio3.jpg')}}" alt="">
-                        </a>    
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="portfolio-tile">
-                        <span class="portfolio-category">
-                            portfolio category
-                        </span>
-                        <div class="portfolio-title-div">
-                            <a href="">
-                                <span class="portfolio-title">
-                                    Portfolio Title BB Roy of great Britain
-                                </span>
-                            </a>
-                        </div>
-                        <a href="" class="portfolio-img">
-                            <img src="{{URL::asset('/images/portfolio/portfolio1.jpg')}}" alt="">
-                        </a>    
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="portfolio-tile">
-                        <span class="portfolio-category">
-                            portfolio category
-                        </span>
-                        <div class="portfolio-title-div">
-                            <a href="">
-                                <span class="portfolio-title">
-                                    Portfolio Title BB Roy of great Britain
-                                </span>
-                            </a>
-                        </div>
-                        <a href="" class="portfolio-img">
-                            <img src="{{URL::asset('/images/portfolio/portfolio2.jpg')}}" alt="">
-                        </a>    
-                    </div>
-                </div>
+                <div class="blog-col">
+                    <a href="">
+                        <article class="blog-list-card">
+                            <div class="blog-list-title">
+                                Custom closet project one
+                            </div>
+                            <img src="{{URL::asset('/images/projects/projects3.jpg')}}" alt="" class="blog-img">
+                            <div class="blog-foot">
+                                <a href="">
+                                    <span class="blog-view-button">View</span>
+                                </a>
+                            </div>
+                        </article>
+                    </a>
+                </div>   
+                <div class="blog-col">
+                    <a href="">
+                        <article class="blog-list-card">
+                            <div class="blog-list-title">
+                                Custom closet project two lorem ipsum
+                            </div>
+                            <img src="{{URL::asset('/images/projects/projects2.jpg')}}" alt="" class="blog-img">
+                            <div class="blog-foot">
+                                <a href="">
+                                    <span class="blog-view-button">View</span>
+                                </a>
+                            </div>
+                        </article>
+                    </a>
+                </div> 
+                <div class="blog-col">
+                    <a href="">
+                        <article class="blog-list-card">
+                            <div class="blog-list-title">
+                                Custom closet project one
+                            </div>
+                            <img src="{{URL::asset('/images/projects/projects3.jpg')}}" alt="" class="blog-img">
+                            <div class="blog-foot">
+                                <a href="">
+                                    <span class="blog-view-button">View</span>
+                                </a>
+                            </div>
+                        </article>
+                    </a>
+                </div>  
+                <div class="blog-col">
+                    <a href="">
+                        <article class="blog-list-card">
+                            <div class="blog-list-title">
+                                Custom closet project two lorem ipsum
+                            </div>
+                            <img src="{{URL::asset('/images/projects/projects1.jpg')}}" alt="" class="blog-img">
+                            <div class="blog-foot">
+                                <a href="">
+                                    <span class="blog-view-button">View</span>
+                                </a>
+                            </div>
+                        </article>
+                    </a>
+                </div>      
             </div>
         </div>
     </section>
