0ae7cff27b80973d3a050e91cbd8668cd065fa53
Amitesh <amitesh.p@chapter247.com>
2020-12-17 18:38:00 +0530

    add rest and change password email template

---
+++ resources/views/dashboard/email/reset_password.blade.php
@@ -0,0 +1,14 @@
+@extends('dashboard.email.master')
+ @section('content')
+
+ <tr>
+ <td style="font-family: 'Lato',sans-serif;">
+    <strong>Hi, <strong>{{ $data->name }}</strong>!</strong>
+    <br><br><br>
+    There was a request to change your password.
+    <br><br>If you didn't make this request, please ignore this email. Otherwise, please click the button below to change your password.
+        <br><br><br><br>
+    <div><a style="background-color:#FF324D;color:#fff;font-size:18px;padding:10px 0;border:solid 1px #FF324D!important;border-radius:4px;text-decoration:none;display:block;width:270px;text-align:center;font-weight:bold;margin:0 auto" href="{{url('/reset-password')}}" target="_blank">Change Password</a></div>
+    </td>
+</tr>
+@endsection
