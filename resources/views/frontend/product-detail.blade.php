51aec62c647d61c7edb8b249a60dff852420f960
harishm247 <rohit.chapter247@gmail.com>
2020-12-21 16:08:41 +0530

    design changes 4pm

--- resources/views/frontend/product-detail.blade.php
+++ resources/views/frontend/product-detail.blade.php
@@ -57,7 +57,9 @@
                         <span class="rating-count-no ml-3"></span>
                         <span>{{$productData['reviewCount']}} Ratings&nbsp;</span>
                         <span class="rating-amp">&amp;</span>
-                        <span>&nbsp;Reviews</span>
+                        <a href="{{ url('#comments') }}">
+                            <span>&nbsp;Reviews</span>
+                        </a>
                     </div>
                     <hr>
                     <p class="pdp-discount-container">
@@ -114,7 +116,7 @@
                         </p>
                         </div>
                         <hr/>
-                        <div class="comments">
+                        <div class="comments" id=comments>
                         @empty($productData['reviewCount'])
                             <h5 class="product_tab_title"> Be the first person to rate this product</h5>
                         @endempty    
