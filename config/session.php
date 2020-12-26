afb07b90cfb5e2ae4dcef997954cc1117d12497f
harishm247 <harish.chapter247@gmail.com>
2020-12-26 16:31:41 +0530

    Debug: check for auth issue on register

--- config/session.php
+++ config/session.php
@@ -153,7 +153,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', null),
+    'domain' => env('SESSION_DOMAIN', 'http://ecom.c247.website'),
 
     /*
     |--------------------------------------------------------------------------
