f28820f8b60fd378f9ed0d4ec28b6d27e4e9bf90
harishm247 <harish.chapter247@gmail.com>
2020-12-26 16:10:29 +0530

    Debug: check for user auth | primary key id

--- app/User.php
+++ app/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
         'is_temp'
     ];
 
-    protected $primaryKey = 'user_id';
+    protected $primaryKey = 'id';
 
     /**
      * The attributes that should be hidden for arrays.
