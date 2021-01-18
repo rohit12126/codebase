6856d4015bc55c3911a4c55df2d36777bf48c353
Shivam Jain <shivam.j@chapter247.com>
2021-01-18 16:00:31 +0530

    anchor tag closing issue

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -203,22 +203,23 @@
         @else
         @foreach ($products as $key => $product)
             <div class="pd-block">
-            @if($product->is_accessory == 0)
-            <a href="{{ url('product/configure/').'/'.$product->slug }}">
-            @else
-            <a href="{{ url('product/detail/').'/'.$product->slug }}">
-            @endif
+
                 <div class="pd-inner">
+                @if($product->is_accessory == 0)
+                    <a href="{{ url('product/configure/').'/'.$product->slug }}">
+                    @else
+                    <a href="{{ url('product/detail/').'/'.$product->slug }}">
+                    @endif
                     <img src="@if(!empty($product->images[0]->image)) {{ url('upload/product/'. $product->images[0]->image)}}  
                     @else https://www.sitzfeldt.com/sites/default/files/styles/menu_453px/public/2-sitzer-sitzfeldt-tom_1.jpg 
                     @endif " class="d-block w-100" alt="...">
+                    </a>
                     @if($product->is_accessory == 0)
                     <a href="{{ url('product/configure/').'/'.$product->slug }}" class="btn btn-fill-out"><i class="linearicons-hammer-wrench"></i> Configure</a>
                     @else
                     <a href="{{ url('product/detail/').'/'.$product->slug }}" class="btn btn-fill-out"><i class="linearicons-eye"></i> Details</a>
                     @endif
                 </div>
-                </a>
             </div>
         @endforeach
         @endif
