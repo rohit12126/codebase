e60d9d2d907fd67227b2feb81f6fa568a7199244
Shivam Jain <shivam.j@chapter247.com>
2021-01-27 16:44:27 +0530

    push changes

--- resources/views/dashboard/product.blade.php
+++ resources/views/dashboard/product.blade.php
@@ -233,7 +233,7 @@
 
 <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.4/summernote.css" rel="stylesheet">
 
-<script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.4/summernote.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.4/summernote.js"></script>
  
 <script>
 
