a333d056a248f8f98bb8483f22e3084c5746dee2
Shivam Jain <shivam.j@chapter247.com>
2021-02-10 11:03:26 +0530

    images

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -23,7 +23,7 @@
         rel="stylesheet"> -->
     <script src="{{asset('js/SimpleComparison_files/iframeResizer.contentWindow.min.js')}}" defer></script>
     <script src="{{ asset('js/js-jquery-1.12.4.min.js')}}" defer></script>
-    <!-- <script src="{{-- asset('select-pure/dist/bundle.min.js'--)}}"></script> -->
+    <!-- <script src="{{-- asset('select-pure/dist/bundle.min.js')--}}"></script> -->
     <style>
     /* Chrome, Safari, Edge, Opera */
         input::-webkit-outer-spin-button,
