b4fa7d080900a3db403d06b5e082bee05f2481ad
harishm247 <rohit.chapter247@gmail.com>
2021-03-31 16:39:10 +0530

    Design changes client page

--- resources/views/frontend/projects.blade.php
+++ resources/views/frontend/projects.blade.php
@@ -84,7 +84,7 @@
                 </div>      
             </div> -->
             <div class="row">
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile">
                         <div class="client-title">
                             The Royale
@@ -94,7 +94,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile">
                         <div class="client-title">
                             City Spire
@@ -104,7 +104,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile">
                         <div class="client-title">
                             Stewart Condominiums – White Plains
@@ -114,284 +114,284 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Doral Hotel</div>
                     <div class="client-name"> Carol Management Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Windsor Court</div>
                     <div class="client-name"> Timko Contracting Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     The Bromley</div>
                     <div class="client-name"> Lehrer-McGovern</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     The Forum</div>
                     <div class="client-name"> Carlyle Development</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Taft Hotel</div>
                     <div class="client-name"> Gotham Construction Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     The Colorado</div>
                     <div class="client-name"> Marson Construction Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Greenwich Apartments</div>
                     <div class="client-name"> Morse/Diesel</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Savoy Towers</div>
                     <div class="client-name"> DeMatteis Organization</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Riverbank West</div>
                     <div class="client-name"> H R H Construction Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Promenade</div>
                     <div class="client-name"> Glick Organization</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Channel Club</div>
                     <div class="client-name"> H R H Construction Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Rockefellar University</div>
                     <div class="client-name"> Tishman Construction Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     The Archives</div>
                     <div class="client-name"> Rockrose Construction Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     72nd Street Residential Project</div>
                     <div class="client-name"> Turner Construction Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Trump Parc</div>
                     <div class="client-name"> H R H Construction Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Trump Plaza</div>
                     <div class="client-name"> H R H Construction Corp..</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Trump Towers</div>
                     <div class="client-name"> H R H Construction Corp</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     New York University Law Dormitory</div>
                     <div class="client-name"> Morse/Diesel</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     The Horizon</div>
                     <div class="client-name"> Glick Organization</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     City Spire</div>
                     <div class="client-name"> Tishman Construction Corp.</div>
                     </div>
                 </div>
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Maxim de Paris Hotel</div>
                     <div class="client-name"> Nova Park, Inc.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Harriet Tubman Houses</div>
                     <div class="client-name"> National Realty Systems</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Battery Park City</div>
                     <div class="client-name"> H R H Construction Corp.</div>
                     </div>
                 </div>
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Marson Construction Corp.</div>
                     <div class="client-name"> Rockrose Construction</div>
                     </div>
                 </div>
-               <div class="col-4">
+               <div class="col-lg-4 col-sm-6">
                    <div class="client-list-tile"><div class="client-title">
                     R. G. Madison Construction</div>
                     <div class="client-name"> Timko Contracting Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Dalton- Greenwich</div>
                     <div class="client-name"> Morse/Diesel</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     United Nations Plaza</div>
                     <div class="client-name"> DeMatteis Organization</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     The Alfred</div>
                     <div class="client-name"> H R H Construction Corp.</div>
                     </div>
                 </div>
 
-               <div class="col-4">
+               <div class="col-lg-4 col-sm-6">
                    <div class="client-list-tile"><div class="client-title">
                     West 85th Street Building Corp.</div>
                     <div class="client-name"> Blitman Building Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Gramercy Court</div>
                     <div class="client-name"> H R H Construction Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Columbus Town Houses</div>
                     <div class="client-name"> Marson Construction Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Tower-in-the-Park</div>
                     <div class="client-name"> Glick Construction Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Parkgate</div>
                     <div class="client-name"> Gotham Construction Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Marlowe</div>
                     <div class="client-name"> 81st Street Realty Co.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Rio</div>
                     <div class="client-name"> H R H Construction Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Infill Housing</div>
                     <div class="client-name"> A.D. Herman Construction</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     James Tower</div>
                     <div class="client-name"> Lefrak Organization</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Cutter Mill Road – Great Neck</div>
                     <div class="client-name"> E. W. Howell Co.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Larchmont Acres - Mamaroneck</div>
                     <div class="client-name"> Gotham Construction Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Stewart Condominiums – White Plains</div>
                     <div class="client-name"> Slavin Contracting Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Life Saver Condominiums –</div>
                     <div class="client-name"> Portchester Pavarini Construction</div>
@@ -399,98 +399,98 @@
                 </div>
                 
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Harborview Terrace – Mamaroneck</div>
                     <div class="client-name"> Construction Equities</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Hofstra University Dormitories</div>
                     <div class="client-name"> Turner Construction Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Oceanside Cove – Oceanside</div>
                     <div class="client-name"> Farrell Court Development Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Colonial T own Houses – Bayside</div>
                     <div class="client-name"> Colonial Town Houses, Inc.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     HIP Cloves Lake Center – Staten Island</div>
                     <div class="client-name"> Gemma Construction Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     The Classic – Greenburgh</div>
                     <div class="client-name"> Slavin Contracting Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     The Centra</div>
                     <div class="client-name"> L. Riso & Sons</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     C.W. Post College Dormitory</div>
                     <div class="client-name"> A. D. Herman</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Union Square</div>
                     <div class="client-name"> Tishman Construction Carlyle Development</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Gregory Commons – Jersey City</div>
                     <div class="client-name"> Battery Park Construction Co.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Cathedral Sq. Housing – Trenton</div>
                     <div class="client-name"> Pike Construction Co.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Newport Towers – Jersey City</div>
                     <div class="client-name"> Lefrak Organization</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Excelsior Apts. – Hackensack</div>
                     <div class="client-name"> Tenwood Construction Corp.</div>
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4 col-sm-6">
                     <div class="client-list-tile"><div class="client-title">
                     Ocean Club – Atlantic City</div>
                     <div class="client-name"> Pavarini Construction</div>
