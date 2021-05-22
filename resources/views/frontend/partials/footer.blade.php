e9a8adbd7c110f4610e6cda76895ef6d4af45adb
Rohit-chapter <rohit.t@chapter247.com>
2021-05-22 19:13:38 +0530

    Footer social icon target

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -8,12 +8,12 @@
   </ul>
   <div class="d-flex w-100 align-items-center mt-4 footer-socialicon-wrapper">
       <span class="footer-socialicon">
-          <a href="{{$setting->facebook_url ?? 'https://www.facebook.com/'}}" title="Connect on Facebook"><img src="{{URL::asset('/images/icon/facebook.png')}}" alt=""></a>
+          <a href="{{$setting->facebook_url ?? 'https://www.facebook.com/'}}" title="Connect on Facebook"><img src="{{URL::asset('/images/icon/facebook.png')}}" target="_blank" alt=""></a>
       </span>
       <span class="footer-socialicon">
-          <a href="{{$setting->instagram_url ?? 'https://www.instagram.com/'}}" title="Follow us on Instagram"><img src="{{URL::asset('/images/icon/instagram.png')}}" alt=""></a>
+          <a href="{{$setting->instagram_url ?? 'https://www.instagram.com/'}}" title="Follow us on Instagram"><img src="{{URL::asset('/images/icon/instagram.png')}}" target="_blank" alt=""></a>
       </span>
       <span class="footer-socialicon">
-          <a href="{{$setting->pinterest_url ?? 'https://www.pinterest.com/'}}" title="Connect on Pinterest"><img src="{{URL::asset('/images/icon/pintrest.png')}}" alt=""></a>
+          <a href="{{$setting->pinterest_url ?? 'https://www.pinterest.com/'}}" title="Connect on Pinterest"><img src="{{URL::asset('/images/icon/pintrest.png')}}" target="_blank" alt=""></a>
       </span>
   </div>
