95a2a6988a1f3a82007142af35e32605a8e3b479
Rohit-chapter <rohit.t@chapter247.com>
2021-06-09 19:53:43 +0530

    Installation page design changes

--- resources/views/frontend/installation.blade.php
+++ resources/views/frontend/installation.blade.php
@@ -1,19 +1,131 @@
 @extends('layouts.front')
 @section('content')
-    <section class="section installation-wrap">
-        <div class="container">
-            <h1 class="pt-0 pb-4 mb-5 heading_s3 text-center">Installation</h1>
-            <p class="mb-4">
-                Installation of CDM closet doors is pretty easy and your level of experience, tools, and door configuration will help dictate whether you will want to do this yourself or hire an installer. Watch our installation videos here for the door type you are interested in and you can decide for yourself if you want to tackle it yourself or hire someone to do it for you.
-            </p>
-            <iframe width="100%" height="520" src="https://www.youtube.com/embed/videoseries?list=PLTR8e6AHZK3vVaSyVrVkUxkRvM3H1u5c3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <section class="section main-wrapper installation-wrap px-0">
+        <h1 class="pt-0 pb-4 mb-5 heading_s3 text-center">Installation</h1>
+        <p class="mb-4">
+            Installation of CDM closet doors is pretty easy and your level of experience, tools, and door configuration will help dictate whether you will want to do this yourself or hire an installer. Watch our installation videos here for the door type you are interested in and you can decide for yourself if you want to tackle it yourself or hire someone to do it for you.
+        </p>
+
+        <div class="installation-videos-wrap">
+            <div class="specs-iframe-wrap" id="youtubeplayer">
+                <iframe class="specs-iframe-responsive" width="100%" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div class="iv-playlist-wrap">
+                <div class="iv-playlist-header">
+                    <div class="iv-playlist-title">
+                        Installation Videos
+                        <span class="ti-close iv-playlist-close"></span>
+                    </div>
+                </div>
+                <ul class="iv-playlist-ul" id="youtubevideotabs">
+                    <li class="iv-playlist-li active" youtubeid="K23rTF9Jfa0">
+                        <div class="iv-pl-icon">
+                            <i class="ion-ios-play"></i>
+                        </div>
+                        <div class="iv-playlist-thumb" style="background-image: url('https://i1.ytimg.com/vi/K23rTF9Jfa0/default.jpg')"></div>
+                        <div class="iv-playlist-thumb-title">Solid Backed Mirror Slider Door</div>
+                    </li>
+                    <li class="iv-playlist-li" youtubeid="NS5CrSbAwi0">
+                        <div class="iv-pl-icon">
+                            <i class="ion-ios-play"></i>
+                        </div>
+                        <div class="iv-playlist-thumb" style="background-image: url('https://i1.ytimg.com/vi/NS5CrSbAwi0/default.jpg');"></div>
+                        <div class="iv-playlist-thumb-title">CDM Bifold Install Overview</div>
+                    </li>
+                    <li class="iv-playlist-li" youtubeid="K23rTF9Jfa0">
+                        <div class="iv-pl-icon">
+                            <i class="ion-ios-play"></i>
+                        </div>
+                        <div class="iv-playlist-thumb" style="background-image: url('https://i1.ytimg.com/vi/K23rTF9Jfa0/default.jpg')"></div>
+                        <div class="iv-playlist-thumb-title">Solid Backed Mirror Slider Door</div>
+                    </li>
+                    <li class="iv-playlist-li" youtubeid="NS5CrSbAwi0">
+                        <div class="iv-pl-icon">
+                            <i class="ion-ios-play"></i>
+                        </div>
+                        <div class="iv-playlist-thumb" style="background-image: url('https://i1.ytimg.com/vi/NS5CrSbAwi0/default.jpg');"></div>
+                        <div class="iv-playlist-thumb-title">CDM Bifold Install Overview</div>
+                    </li>
+                    <li class="iv-playlist-li" youtubeid="K23rTF9Jfa0">
+                        <div class="iv-pl-icon">
+                            <i class="ion-ios-play"></i>
+                        </div>
+                        <div class="iv-playlist-thumb" style="background-image: url('https://i1.ytimg.com/vi/K23rTF9Jfa0/default.jpg')"></div>
+                        <div class="iv-playlist-thumb-title">Solid Backed Mirror Slider Door</div>
+                    </li>
+                    <li class="iv-playlist-li" youtubeid="NS5CrSbAwi0">
+                        <div class="iv-pl-icon">
+                            <i class="ion-ios-play"></i>
+                        </div>
+                        <div class="iv-playlist-thumb" style="background-image: url('https://i1.ytimg.com/vi/NS5CrSbAwi0/default.jpg');"></div>
+                        <div class="iv-playlist-thumb-title">CDM Bifold Install Overview</div>
+                    </li>
+                    <li class="iv-playlist-li" youtubeid="K23rTF9Jfa0">
+                        <div class="iv-pl-icon">
+                            <i class="ion-ios-play"></i>
+                        </div>
+                        <div class="iv-playlist-thumb" style="background-image: url('https://i1.ytimg.com/vi/K23rTF9Jfa0/default.jpg')"></div>
+                        <div class="iv-playlist-thumb-title">Solid Backed Mirror Slider Door</div>
+                    </li>
+                    <li class="iv-playlist-li" youtubeid="NS5CrSbAwi0">
+                        <div class="iv-pl-icon">
+                            <i class="ion-ios-play"></i>
+                        </div>
+                        <div class="iv-playlist-thumb" style="background-image: url('https://i1.ytimg.com/vi/NS5CrSbAwi0/default.jpg');"></div>
+                        <div class="iv-playlist-thumb-title">CDM Bifold Install Overview</div>
+                    </li>
+                    <li class="iv-playlist-li" youtubeid="K23rTF9Jfa0">
+                        <div class="iv-pl-icon">
+                            <i class="ion-ios-play"></i>
+                        </div>
+                        <div class="iv-playlist-thumb" style="background-image: url('https://i1.ytimg.com/vi/K23rTF9Jfa0/default.jpg')"></div>
+                        <div class="iv-playlist-thumb-title">Solid Backed Mirror Slider Door</div>
+                    </li>
+                    <li class="iv-playlist-li" youtubeid="NS5CrSbAwi0">
+                        <div class="iv-pl-icon">
+                            <i class="ion-ios-play"></i>
+                        </div>
+                        <div class="iv-playlist-thumb" style="background-image: url('https://i1.ytimg.com/vi/NS5CrSbAwi0/default.jpg');"></div>
+                        <div class="iv-playlist-thumb-title">CDM Bifold Install Overview</div>
+                    </li>
+                    <li class="iv-playlist-li" youtubeid="K23rTF9Jfa0">
+                        <div class="iv-pl-icon">
+                            <i class="ion-ios-play"></i>
+                        </div>
+                        <div class="iv-playlist-thumb" style="background-image: url('https://i1.ytimg.com/vi/K23rTF9Jfa0/default.jpg')"></div>
+                        <div class="iv-playlist-thumb-title">Solid Backed Mirror Slider Door</div>
+                    </li>
+                    <li class="iv-playlist-li" youtubeid="NS5CrSbAwi0">
+                        <div class="iv-pl-icon">
+                            <i class="ion-ios-play"></i>
+                        </div>
+                        <div class="iv-playlist-thumb" style="background-image: url('https://i1.ytimg.com/vi/NS5CrSbAwi0/default.jpg');"></div>
+                        <div class="iv-playlist-thumb-title">CDM Bifold Install Overview</div>
+                    </li>
+                </ul>
+            </div>
         </div>
     </section>
-
+    
 @endsection
 
 @section('scripts')
+    <script>
+        $(function(){
+            console.log('hiasda');
+            var youtubeUrl = "https://www.youtube.com/embed/" + $('#youtubevideotabs li:first-child').attr('youtubeid') + "?rel=0";
+            $('#youtubeplayer iframe').attr('src',youtubeUrl);
+            
+            $('#youtubevideotabs li').click(function(){
+                youtubeUrl = "https://www.youtube.com/embed/"+ $(this).attr('youtubeid') +"?rel=0";
+                $('#youtubeplayer iframe').attr('src',youtubeUrl);
+                console.log('hiasda1');
 
+                $('#youtubevideotabs li').removeClass('active');
+                $(this).addClass('active');
+                console.log('hiasda');
 
-
+            }); 
+        });
+    </script>
 @endsection
