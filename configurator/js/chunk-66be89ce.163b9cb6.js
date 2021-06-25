32b111d6b8b93dcd8b819052eb2b3693409b0421
Shivam Jain <shivam.j@chapter247.com>
2021-06-25 17:24:10 +0530

    push

--- configurator/js/chunk-66be89ce.163b9cb6.js
+++ configurator/js/chunk-66be89ce.163b9cb6.js
@@ -409,7 +409,7 @@
                                 parts : i.fullList[0].parameters
                             })
                           };
-                          const tr = await fetch("http://localhost/c247/codebase/index.php/product/price", requestOptions);
+                          const tr = await fetch("configurator/product/price", requestOptions);
                           const price = await tr.json();
                         this._configuratorUiCallbacks.onUpdatePrice('$', price)
                     }
