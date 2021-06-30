b07b4b455960af44e46467a5a60f1ca2ef87443e
shivam jain <shivam.j@chapter247.com>
2021-06-30 15:55:10 +0530

    profile change

--- resources/views/frontend/partials/profile.blade.php
+++ resources/views/frontend/partials/profile.blade.php
@@ -29,6 +29,22 @@
                         <div class="error">{{ $errors->first('mobile') }}</div>
                     @endif
                 </div>
+                @if(empty($user->password))
+                <div class="form-group col-md-12">
+                    <label>Create Password <span class="mandatory">*</span></label>
+                    <input required class="form-control" name="password" type="password" min="8">
+                    @if($errors->has('password'))
+                    <div class="text-danger">{{ $errors->first('password') }}</div>
+                    @endif
+                </div>
+                <div class="form-group col-md-12">
+                    <label>Confirm Password <span class="mandatory">*</span></label>
+                    <input required class="form-control"  name="password_confirmation" type="password" min="8">
+                    @if($errors->has('password_confirmation'))
+                    <div class="text-danger">{{ $errors->first('password_confirmation') }}</div>
+                    @endif
+                </div>
+                @endif
                 <input type="hidden" name="id" value="{{ $user->id }}" required>
                 
                 <div class="col-md-12">
