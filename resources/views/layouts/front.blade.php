c97c856db6bfb52088114de8a7088c64baec6029
shivam jain <shivam.j@chapter247.com>
2021-07-13 13:15:53 +0530

    push changes in matrix

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -7,18 +7,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="Anil z" name="author">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    
+    <title>Custom Closet Doors NYC | United States | Custom Door and Mirror</title>
+    <meta name="description" content="Custom Door and Mirror is your one stop online store to buy residential and commercial closet doors and hardware. Visit our website and browse huge range of doors!"/>
+    <link rel="canonical" href="https://www.customdoorandmirror.com"/>
+    <meta name="keywords" content="Custom Doors Near Me, Custom Closet Doors NYC"/>    
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <meta property="og:url" content="{{url()->current()}}"/>
-    <meta property="og:type" content="article" />
-    <meta property="og:title" content="{{$blog->title ?? ''}}"/>
-    <meta property="og:description" content="{!! Str::limit( $blog->description ?? '' , 200, ' ...') !!}"/>
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="{{$blog->title ?? 'Custom Closet Doors NYC | United States | Custom Door and Mirror'}}"/>
+    <meta property="og:description" content="Custom Door and Mirror is your one stop online store to buy residential and commercial closet doors and hardware. Visit our website and browse huge range of doors!"/>
     <meta property="og:image" content="@if(isset($blog)){{ asset('upload/blog/'.$blog->image)}}@else{{asset('upload/images/home/step-two.jpg')}}@endif"/>
+    <meta property="og:site_name" content="customdoorandmirror"/>
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="twitter:title" content="Custom Closet Doors NYC | United States | Custom Door and Mirror"/>
+    <meta name="twitter:description" content="Custom Door and Mirror is your one stop online store to buy residential and commercial closet doors and hardware. Visit our website and browse huge range of doors!"/>
+    <meta name="twitter:image" content="@if(isset($blog)){{ asset('upload/blog/'.$blog->image)}}@else{{asset('upload/images/home/step-two.jpg')}}@endif"/>
+
+
     <!-- SITE TITLE -->
-    <title>Custom Door and Mirror</title><!-- Favicon Icon -->
+    
+
     <link rel="shortcut icon" type="image/png" href="{{ asset('assets/favicon/favicon.jpg')}}"><!-- Animation CSS -->
     <!-- <link rel="stylesheet" href="{{ asset('css/css-animate.css')}}">Latest Bootstrap min CSS -->
     <link rel="stylesheet" href="{{ asset('css/css-bootstrap.min.css')}}">
@@ -29,6 +37,16 @@
     <script src="{{asset('js/SimpleComparison_files/iframeResizer.contentWindow.min.js')}}" defer></script>
     <script src="{{ asset('js/js-jquery-1.12.4.min.js')}}"></script>
     <!-- <script src="{{ asset('select-pure/dist/bundle.min.js')}}"></script> -->
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121622888-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-121622888-1');
+    </script>
     <style>
     /* Chrome, Safari, Edge, Opera */
         input::-webkit-outer-spin-button,
